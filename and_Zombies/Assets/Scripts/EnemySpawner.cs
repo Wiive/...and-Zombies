684534b01c5967a36a386a54ef12241e593580e2
Wiive <wirrechan@hotmail.com>
2021-02-04 10:01:25 +0100

    Added comments

--- and_Zombies/Assets/Scripts/EnemySpawner.cs
+++ and_Zombies/Assets/Scripts/EnemySpawner.cs
@@ -6,7 +6,7 @@ public class EnemySpawner : MonoBehaviour
 
     [SerializeField] float spawnTimer = 3;
 
-    public enum SpawnSide { horizontal, vertical}
+    public enum SpawnSide { horizontal, vertical}                                            //Decides if the Zombies gonna spawn on the right side or under the screen
     public SpawnSide spawnSide;
 
     private void Update()
@@ -14,14 +14,14 @@ public class EnemySpawner : MonoBehaviour
         spawnTimer -= 1 * Time.deltaTime;
         if ((spawnTimer < 0) && (spawnSide == SpawnSide.vertical))
         {
-            Vector3 position = new Vector3(gameObject.transform.position.x, (Random.Range(-100f, -360f)), 0);
+            Vector3 position = new Vector3(gameObject.transform.position.x, (Random.Range(-100f, -360f)), 0);  //Sets a random y position from the spawners x position
             Instantiate(enemyPrefab, position, Quaternion.identity);
             spawnTimer = 3;
         }
 
         else if ((spawnTimer < 0) && (spawnSide == SpawnSide.horizontal))
         {
-            Vector3 position = new Vector3(Random.Range(-250f, 250f), gameObject.transform.position.y, 0);
+            Vector3 position = new Vector3(Random.Range(-250f, 250f), gameObject.transform.position.y, 0);    //Sets a random x position from the spawners y position
             Instantiate(enemyPrefab, position, Quaternion.identity);
             spawnTimer = 3;
         }
