5cc339cfdb681510876ece3b44b5928c51805446
Pablo <533536@student.glu.nl>
2021-02-04 11:12:33 +0100

    TOILET PAPER

--- and_Zombies/Assets/Scripts/ItemType.cs
+++ and_Zombies/Assets/Scripts/ItemType.cs
@@ -12,9 +12,13 @@ public class ItemType : MonoBehaviour
 
     private void Start()
     {
-        itemID = Random.Range(0, 2);
+        itemID = Random.Range(0, 3);
         spriteRenderer = GetComponent<SpriteRenderer>();
-        
+
+        if (itemID == 3)
+        {
+            itemID = 2;
+        }
         if (itemID == 0)
         {
             spriteRenderer.sprite = ItemSprite[0];
@@ -27,6 +31,7 @@ public class ItemType : MonoBehaviour
         {
             spriteRenderer.sprite = ItemSprite[2];
         }
+        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
